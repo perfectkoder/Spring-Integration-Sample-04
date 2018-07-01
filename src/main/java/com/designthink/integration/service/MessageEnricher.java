47a6a84911eb0f5458355d01bf4fe92282d35117
perfectkoder <er.umeshverma@gmail.com>
2018-07-01 14:38:21 +0530

    Initial Commit

--- src/main/java/com/designthink/integration/service/MessageEnricher.java
+++ src/main/java/com/designthink/integration/service/MessageEnricher.java
@@ -6,7 +6,6 @@ import org.springframework.messaging.Message;
 public class MessageEnricher {
 
     public Message<String> enrichMessage(Message<String> message){
-        System.out.println("The input Message" + message.getPayload());
         return MessageBuilder.withPayload("Reply for message " + message.getHeaders().get("messageNo")).build();
     }
 }
